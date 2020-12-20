96972b128ce70e56917f711fd88f77aa52b588aa
Miguel Peña <mpena@universaldoctor.com>
2020-12-20 18:11:52 +0100

    Creada MainScreen

---
+++ lib/src/main_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  final tabs = [
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.red,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.indigo,
+        selectedFontSize: 20,
+        unselectedFontSize: 15,
+        iconSize: 30,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: "Tasks",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: "Programmers",
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
